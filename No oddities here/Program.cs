78737279e6cff9970b6c52e83894fa3b48c8004c
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-11 20:57:08 +0300

    Добавьте файлы проекта.

---
+++ No oddities here/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace No_oddities_here
+{
+    class Program
+    {
+        public static int[] NoOdds(int[] values)
+        {
+
+            List<int> list = new List<int>();
+            for (int i = 0; i < values.Length; i++)
+            {
+                int value = values[i];
+                if (value % 2 == 0)
+                {
+                    
+                    list.Add(value);
+                }
+            }
+            
+            return list.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Square(n) Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Square_n__Sum
+{
+    class Program
+    {
+        public static int SquareSum(int[] n)
+        {
+            double res = 0;
+            for (int i = 0; i < n.Length; i++)
+            {                
+                res += Math.Pow(n[i],2);
+            }        
+                        
+            return (int)res;
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
