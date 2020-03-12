35972e9d8d85abbb6126d992e402dae3d7fe9aad
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-12 19:51:55 +0300

    Not solved

--- Find the first non-consecutive number/Program.cs
+++ Find the first non-consecutive number/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace Find_the_first_non_consecutive_number
 {
-    class Program
+    class Kata
     {
+        public static object FirstNonConsecutive(int[] arr)
+        {
+            
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                if (list.IndexOf(arr[i]) != arr[i-1])
+                {
+                    list.Add(arr[i]);
+                    
+                    
+                }
+                
+            }
+            return list.ToArray();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Assert.AreEqual(6, Kata.FirstNonConsecutive(new int[] { 1, 2, 3, 4, 6, 7, 8 }));
+           
         }
     }
 }
