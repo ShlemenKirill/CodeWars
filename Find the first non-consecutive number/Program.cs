03c6b83c472889bdf06d02962c4b4dd7ef5ee16c
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-12 17:52:33 +0300

    Create project

---
+++ Find the first non-consecutive number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Find_the_first_non_consecutive_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
