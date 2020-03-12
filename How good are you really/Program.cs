4fbe774adfea49912e398d52880907c4bd15d98c
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-12 19:53:50 +0300

    New project

---
+++ How good are you really/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace How_good_are_you_really
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
