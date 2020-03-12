857dc8378ca0da26af01d5136f1f43179b29eef5
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-12 20:24:15 +0300

    Solved

--- How good are you really/Program.cs
+++ How good are you really/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace How_good_are_you_really
 {
     class Program
     {
+        public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+        {
+            int average = 0;
+            int length = ClassPoints.Length;            
+            
+            for (int i = 0; i < ClassPoints.Length; i++)
+            {
+                average =+ ClassPoints[i];
+                
+            }
+
+            if ((average + YourPoints)/(length) < YourPoints)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
