43a5564f07d8d17f59e5a9c7b7437e6fdcd2a36e
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-14 23:33:23 +0300

    Add project

---
+++ Convert number to reversed array of digits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Convert_number_to_reversed_array_of_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
