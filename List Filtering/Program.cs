bbc332fb31722889f4720b8b46571846e2edcde1
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-16 00:56:10 +0300

    Corrected

--- List Filtering/Program.cs
+++ List Filtering/Program.cs
@@ -1,9 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace List_Filtering
 {
