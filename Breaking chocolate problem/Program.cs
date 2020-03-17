c3ae920c86812b4363c0a2ac0bbd33a09fa86f41
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-17 22:31:03 +0300

    Solved

---
+++ Breaking chocolate problem/Program.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Breaking_chocolate_problem
+{
+    class Kata
+    {
+        public static int BreakChocolate(int n, int m)
+        {
+            if (n == 1 && m == 1)
+            {
+                return 0;
+            }
+            if ( m==0|| n==0)
+            {
+                return 0;
+            }
+            return n * m - 1;
+        }
+        static void Main(string[] args)
+        {
+            Assert.AreEqual(24, Kata.BreakChocolate(5, 5));
+            Assert.AreEqual(0, Kata.BreakChocolate(1, 1));
+        }
+    }
+}
