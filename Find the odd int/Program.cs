a79fe96724ac81bf62542a3b2231af9d354304b7
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-17 22:32:33 +0300

    Add project

---
+++ Find the odd int/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Find_the_odd_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
