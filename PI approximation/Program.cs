48267921da5f11a515d50d1319df857a7aa25e3a
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-28 01:23:30 +0300

    Solved

---
+++ PI approximation/Program.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+
+namespace PI_approximation
+{
+    public class PiApprox
+    {
+        public static ArrayList iterPi(double epsilon)
+        {
+            double iter = (double)(1/epsilon);
+            double pi = 0;
+            double sign = 1;            
+            
+            for (double i = 0; i < 1/epsilon; i++)
+            {
+                pi += sign / (2 * i + 1);
+                sign = -sign;
+            }
+            double resPI = 4 * pi;
+            double er = Math.Round(resPI, 10);           
+            ArrayList res = new ArrayList() {iter, er};
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList r = PiApprox.iterPi(0.1);
+            ArrayList c = new ArrayList { 10, 3.0418396189 };
+            Assert.AreEqual(c, r);
+        }
+    }
+}
